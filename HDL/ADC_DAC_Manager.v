7fbd4181267ae88e496ecce2bcc7618c3fc23b79
Ben Guan <42010432+bguan2022@users.noreply.github.com>
2022-09-06 14:32:55 -0700

    Update ADC_DAC_Manager.v

--- HDL/ADC_DAC_Manager.v
+++ HDL/ADC_DAC_Manager.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/13/2019 04:04:25 PM
-// Design Name: 
-// Module Name: ADC_DAC_Manager
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module ADC_DAC_Controller
