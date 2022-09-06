685d11af7c65187ed772def57daeb436fe14bc3f
Ben Guan <42010432+bguan2022@users.noreply.github.com>
2022-09-06 14:26:53 -0700

    Update and rename ram.v to HDL/ram.v

--- HDL/ram.v
+++ HDL/ram.v
@@ -1,23 +1,5 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 09/26/2019 02:29:33 PM
-// Design Name: 
-// Module Name: ram
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module ram # 
