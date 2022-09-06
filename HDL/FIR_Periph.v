714b757a991efa8b7b302e43e54c08978b515c5f
Ben Guan <42010432+bguan2022@users.noreply.github.com>
2022-09-06 14:26:39 -0700

    Update and rename FIR_Periph.v to HDL/FIR_Periph.v

--- HDL/FIR_Periph.v
+++ HDL/FIR_Periph.v
@@ -1,23 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 11/07/2019 02:17:17 PM
-// Design Name: 
-// Module Name: FIR_Periph
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
 
 module FIR_Periph #
